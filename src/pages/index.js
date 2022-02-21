2bbc009288041ed4277d29949917275eef339008
waxingmedia <brian@waxingmedia.com>
2022-02-21 12:06:12 -0700

    3

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ import Scroll from '../components/Scroll';
 
 const sections = [
   { id: 'top', name: 'Intro', icon: 'fa-home' },
-  { id: 'portfolio', name: 'Logos', icon: 'fa-th' },
+  { id: 'logos', name: 'Logos', icon: 'fa-th' },
   { id: 'about', name: 'Color', icon: 'fa-user' },
 
 ];
@@ -29,16 +29,14 @@ const IndexPage = () => (
         <div className="container">
           <header>
             <h2 className="alt">
-              Hi! I'm <strong>Anubhav</strong>
-              <br />
-              Web developer
+              Videstra
             </h2>
-            <p>I have made few things check it out.</p>
+            <p>Logos and Branding</p>
           </header>
 
           <footer>
             <Scroll type="id" element={'portfolio'}>
-              <a href="#portfolio" className="button">
+              <a href="#logos" className="button">
                 Show me
               </a>
             </Scroll>
@@ -46,18 +44,12 @@ const IndexPage = () => (
         </div>
       </section>
 
-      <section id="portfolio" className="two">
+      <section id="logos" className="two">
         <div className="container">
           <header>
-            <h2>Portfolio</h2>
+            <h2>Logos</h2>
           </header>
 
-          <p>
-            Life will feel it is always a great need for eu valley, the valley
-            CNN ridiculous smile at any time chat mainstream clinical homes.
-            Mauris floor was very warm and we need it. One customer now nibh
-            Bureau dark pools behavior.
-          </p>
 
           <div className="row">
             <div className="col-4 col-12-mobile">
@@ -118,60 +110,20 @@ const IndexPage = () => (
         </div>
       </section>
 
-      <section id="about" className="three">
+      <section id="colors" className="three">
         <div className="container">
           <header>
-            <h2>About Me</h2>
+            <h2>Colors</h2>
           </header>
 
           <a href="/#" className="image featured">
             <img src={pic8} alt="" />
           </a>
 
-          <p>
-            Developers football competition in diameter big price to layer the
-            pot. Chavez ultricies care who wants to CNN. Lobortis elementum
-            aliquet eget a den of which they do not hold it in hatred developers
-            nor the mountains of the deposit slip. The element of time, sem ante
-            ullamcorper dolor nulla quam placerat viverra environment is not
-            with our customers. Free makeup and skirt until the mouse or
-            partners or to decorate each targeted.
-          </p>
         </div>
       </section>
 
-      <section id="contact" className="four">
-        <div className="container">
-          <header>
-            <h2>Contact</h2>
-          </header>
-
-          <p>
-            The element of time, sem ante ullamcorper dolor nulla quam placerat
-            viverra environment is not with our customers. Free makeup and skirt
-            until the mouse. Japan this innovative and ultricies carton salad
-            clinical ridiculous now passes from enhanced. Mauris pot innovative
-            care for my pain.
-          </p>
-
-          <form method="post" action="#">
-            <div className="row">
-              <div className="col-6 col-12-mobile">
-                <input type="text" name="name" placeholder="Name" />
-              </div>
-              <div className="col-6 col-12-mobile">
-                <input type="text" name="email" placeholder="Email" />
-              </div>
-              <div className="col-12">
-                <textarea name="message" placeholder="Message" />
-              </div>
-              <div className="col-12">
-                <input type="submit" value="Send Message" />
-              </div>
-            </div>
-          </form>
-        </div>
-      </section>
+     
     </div>
 
     <PageFooter />

a92e8050f0ac2379377a1030cbf329ca2657cb3b
waxingmedia <brian@waxingmedia.com>
2022-02-21 12:03:03 -0700

    2

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
   pathPrefix: `/gatsby-starter-prologue/`, // This path is subpath of your hosting https://domain/portfolio
-  authorName: 'Anubhav',
-  heading: 'Web Developer',
+  authorName: 'Waxingmedia',
+  heading: 'Videstra Branding',
 
 };
